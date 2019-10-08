dcc2e27ac5b3e9d3381cfbce72f343ab5a7d20a7
guhsanchez <sanchez.github@gmail.com>
2019-10-08 10:57:09 -0300

    adc do software 1.0

---
+++ Software/WpfApp1/ClassesControle/BL/PedidoBL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesControle.BL
+{
+    public class PedidoBL
+    {
+        private static PedidoBL instancia = null;
+        private PedidoBL() { }
+        public static PedidoBL getinstancia()
+        {
+            if (instancia == null)
+                instancia = new PedidoBL();
+            return instancia;
+        }
+    }
+}
---
+++ Software/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassesControle.DAO;
+using ClassesModel;
+using ClassesModel.DTO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLogar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //UsuarioDto porra = ClassesControle.Controller.Controller.getinstancia().RealizaLogin(txbSenha.Text);
+                Escolha frm = new Escolha();
+                this.Hide();
+                frm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
